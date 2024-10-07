245ad795ee5f93144fe1c922878d3911c66d2def
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2024-10-07 22:07:05 +0300

    Randomly rotate Pizduk and place credits on walls.

--- Assets/_LudumDare56/Scripts/PizdukManager.cs
+++ Assets/_LudumDare56/Scripts/PizdukManager.cs
@@ -1,8 +1,12 @@
-using Cysharp.Threading.Tasks.Triggers;
 using UnityEngine;
 
 public class PizdukManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        gameObject.transform.localRotation = Quaternion.Euler(0,  Random.Range(0, 180), 0);
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         GamePlayManager.Instance.GameOver();
